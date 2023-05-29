c453d7c512b8bbca3b2831221e3f6e1448d5bc5f
Petr <mihailinpk@yandex.ru>
2023-05-29 17:07:25 +0300

    init commit

---
+++ src/main/java/su/mytestprojects/h2learn/H2LearnApplication.java
@@ -0,0 +1,13 @@
+package su.mytestprojects.h2learn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class H2LearnApplication	{
+
+	public static void main(String[] args) {
+		SpringApplication.run(H2LearnApplication.class, args);
+	}
+
+}
---
+++ src/test/java/su/mytestprojects/h2learn/H2LearnApplicationTests.java
@@ -0,0 +1,13 @@
+package su.mytestprojects.h2learn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class H2LearnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
